a6b4fde4f324785928a62b0b8f64fb96febf4374
Martin Carianni <7013483+spiez@users.noreply.github.com>
2023-03-25 14:55:10 +0100

    Update roomDetector.py

--- scripts/roomDetector.py
+++ scripts/roomDetector.py
@@ -10,10 +10,13 @@ from io import BytesIO
 import base64
 
 def decode_base64_to_image(encoding):
+    print('checkpoint A')
     if encoding.startswith("data:image/"):
         encoding = encoding.split(";")[1].split(",")[1]
+    print('checkpoint B')
 
     image = Image.open(BytesIO(base64.b64decode(encoding)))
+    print('checkpoint C')
     return image
 
 
@@ -36,9 +39,15 @@ class Script(scripts.Script):
         if isinstance(img, str): 
             pil_img = decode_base64_to_image(img)
         img_rgb = pil_img.convert('RGB')
+        print('checkpoint 1')
         
         processed = shared.interrogator.interrogate(img_rgb)
         
+        print('checkpoint 2')
+        
         for i in range(len(prompts)):
             prompts[i] = prompts[i].replace('CLIP_REPLACEME', processed)
+            print('checkpoint 3')
             print(prompts[i])
+            
+        print('checkpoint 4')
